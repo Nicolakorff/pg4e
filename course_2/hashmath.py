005d9af5cee874577d4cc37495902c665e86feb7
Nicola Korff <nicolavonkorff@gmail.com>
2024-12-23 14:07:24 +0100

    Add files via upload

---
+++ course_2/hashmath.py
@@ -0,0 +1,10 @@
+
+# https://www.pg4e.com/code/hashmath.py
+
+x = 15
+y = ord('H')
+print('x', x, format(x, '08b'))
+print('y', y, format(y, '08b'))
+print('x^y ', format(x^y, '08b'));
+print('x&y ', format(x&y, '08b'));
+print('x<<1', format(x<<1, '08b'));
